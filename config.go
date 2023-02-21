4fa2904f1ebc78d4181b1231d844ccb1e1489e6d
Jay Damon <jay.n.damon@gmail.com>
2023-02-20 19:11:44 -0500

    Add initial config as used in the accountLinkService

---
+++ config.go
@@ -0,0 +1,64 @@
+package moneymakerplaid
+
+import (
+	"fmt"
+	"github.com/plaid/plaid-go/plaid"
+	"log"
+	"os"
+)
+
+var environments = map[string]plaid.Environment{
+	"sandbox":     plaid.Sandbox,
+	"development": plaid.Development,
+	"production":  plaid.Production,
+}
+
+type Config struct {
+	Client       *plaid.APIClient
+	Config       *plaid.Configuration
+	Products     string
+	CountryCodes string
+	RedirectUrl  string
+}
+
+func NewConfiguration() *Config {
+
+	plaidClientId := getOrExit("PLAID_CLIENT_ID")
+	plaidSecret := getOrExit("PLAID_SECRET")
+	plaidEnv := getOrDefault("PLAID_ENV", "sandbox")
+	plaidProducts := getOrDefault("PLAID_PRODUCTS", "transactions")
+	plaidCountryCodes := getOrDefault("PLAID_COUNTRY_CODES", "US")
+	plaidRedirectUri := getOrDefault("PLAID_REDIRECT_URI", "")
+
+	config := plaid.NewConfiguration()
+	config.AddDefaultHeader("PLAID-CLIENT-ID", plaidClientId)
+	config.AddDefaultHeader("PLAID-SECRET", plaidSecret)
+	config.UseEnvironment(environments[plaidEnv])
+	// config.Debug = true
+
+	client := plaid.NewAPIClient(config)
+
+	return &Config{
+		Client:       client,
+		Config:       config,
+		Products:     plaidProducts,
+		CountryCodes: plaidCountryCodes,
+		RedirectUrl:  plaidRedirectUri,
+	}
+}
+
+func getOrExit(envVar string) string {
+	val := os.Getenv(envVar)
+	if val == "" {
+		log.Fatal(fmt.Printf("%s is not set. Make sure to fill out the .env file", envVar))
+	}
+	return val
+}
+
+func getOrDefault(envVar string, defaultVal string) string {
+	val := os.Getenv(envVar)
+	if val == "" {
+		return defaultVal
+	}
+	return val
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jaydamon/moneymakerplaid
 
 go 1.19
+
+require github.com/plaid/plaid-go v1.10.0 // indirect
